3d26c541d2930cabd49bd30ea668be9e2e8b4939
LaraFrancken <159938682+LaraFrancken@users.noreply.github.com>
2025-08-11 16:33:34 +0200

    Update and rename STREAM-PRS.bash to STREAM-PRS_GSx.bash

--- scripts/STREAM-PRS_GSx.bash
+++ scripts/STREAM-PRS_GSx.bash
@@ -12,14 +12,15 @@ PRScs_path=""
 binary_trait="TRUE" #(TRUE=binary, FALSE=continuous)
 cross_validation="FALSE" #(TRUE=use cross validation, FALSE=don't use cross validation)
 covariates="" #Comma separated list of covariates to include in regression model e.g. "Age+Sex,Sex" will do regression analysis for PHENO ~ PRS + Age+Sex and for PHENO ~ PRS + Sex; if e.g. "Age,Sex" it will do regression analysis for PHENO ~ PRS + Age and for PHENO ~ PRS + Sex It will NOT do PHENO ~ PRS + Age + Sex; leave empty if you have no covariates.
+gene_set="FALSE" #(TRUE=yes, FALSE=no)
 
 #Output path
-out="."
+out=""
 
 #GWAS summary statistics
 #If chr_pos_ID and rsID are in the same file, fill in the same GWAS twice
 GWAS_file=""
-GWAS_file_rsID="" 
+GWAS_file_rsID=""
 
 #GWAS column names
 snp_col=""
@@ -58,6 +59,18 @@ PC_test=""
 
 #Covariate file (FID, IID, cov_1, cov_2, ..., cov_N): this file is optional
 cov_file=""
+
+#Gene set file (Name set, URL, Genes): this file is optional
+#Make sure there is no missing newline at end of the file!
+MSigDB=""
+#GTF file with the genome boundary of the genetic elements within the human genome: this file is optional
+GTF=""
+#Gene boundary padding: padding to add upstream (5') and downstream (3') to the gene region. 
+#Values are in kilobases (kb).
+pad5="5"
+pad3="10"
+#"Base" is a file containing the SNPs of all gene sets together
+base="TRUE" #(TRUE=make "Base" gene set, FALSE=no need for "Base")
 
 #Cores
 cores="1"
@@ -103,6 +116,26 @@ mkdir -p ${out_LDpred2}
 mkdir -p ${out_lasso2}
 mkdir -p ${out_comparison}
 
+if [ "$gene_set" == "TRUE" ]; then
+    out_SNPs_per_set=${out}/SNPS_per_set
+    out_PRSet=${out}/PRSet
+    out_PRSice_on_Set=${out}/PRSice_on_set
+    out_lassosum_on_set=${out}/lassosum_on_set
+    out_PRScs_on_set=${out}/PRScs_on_set
+    out_LDpred2_on_set=${out}/LDpred2_on_set
+    out_lasso2_on_set=${out}/lasso2_on_set
+fi
+
+if [ "$gene_set" == "TRUE" ]; then
+    mkdir -p ${out_SNPs_per_set}
+    mkdir -p ${out_PRSet}
+    mkdir -p ${out_PRSice_on_Set}
+    mkdir -p ${out_lassosum_on_set}
+    mkdir -p ${out_PRScs_on_set}
+    mkdir -p ${out_LDpred2_on_set}
+    mkdir -p ${out_lasso2_on_set}
+fi
+
 #GWAS summary statistics
 edited_GWAS_file=${GWAS_file}_edited_by_pipeline
 edited_GWAS_for_PRS_CS=${GWAS_file}_edited_by_pipeline_for_PRS_CS
@@ -113,7 +146,7 @@ edited_GWAS_for_LDpred2=${GWAS_file}_edited_by_pipeline_for_LDpred2
 #Edit GWAS
 Rscript edit_GWAS.r "$GWAS_file" "$GWAS_file_rsID" "$p_value_col" "$beta_col" "$snp_col" "$rsID_col" "$effect_allele" "$noneffect_allele" "$pos_col"
 
-##Run PRSice
+#Run PRSice
 ./prsice.bash "$PRSice_path" "$edited_GWAS_file" "$training_file" "$training_file_prefix" "$snp_col" "$chromosome_col" "$pos_col" "$effect_allele" "$noneffect_allele" "$clumpkb" "$clumpp" "$clumpr2" "$pval_thresholds" "$out_PRSice" "$test_file" "$test_file_prefix"
 
 #Run lassosum
@@ -134,3 +167,45 @@ else
 	echo "Indicate if trait is binary (TRUE) or not (FALSE)"
 fi
 
+#Run gene-set extension scripts
+
+#SNP extraction
+if [ "$gene_set" == "TRUE" ]; then
+    ./get_SNPs_per_set.bash "$MSigDB" "$GTF" "$out_SNPs_per_set" "$training_file" "$training_file_rsID" "$pad5" "$pad3" "$base"
+fi
+
+#Run PRSet
+if [ "$gene_set" == "TRUE" ]; then
+    ./PRSet.bash "$PRSice_path" "$edited_GWAS_file" "$MSigDB" "$GTF" "$training_file" "$training_file_prefix" "$snp_col" "$chromosome_col" "$pos_col" "$effect_allele" "$noneffect_allele" "$clumpkb" "$clumpp" "$clumpr2" "$pval_thresholds" "$out_PRSet" "$test_file" "$test_file_prefix" "$out_comparison"
+fi
+
+#Run PRSice on every gene set
+if [ "$gene_set" == "TRUE" ]; then
+   ./PRSice_on_set.bash "$PRSice_path" "$edited_GWAS_file" "$MSigDB" "$GTF" "$training_file" "$training_file_prefix" "$snp_col" "$chromosome_col" "$pos_col" "$effect_allele" "$noneffect_allele" "$clumpkb" "$clumpp" "$clumpr2" "$pval_thresholds" "$out_PRSice_on_Set" "$test_file" "$test_file_prefix" "$out_comparison" "$out_SNPs_per_set"
+fi
+
+#Run lassosum on every gene set
+if [ "$gene_set" == "TRUE" ]; then
+   ./lassosum_on_set.bash "$test_file_prefix" "$test_file" "$edited_GWAS_file" "$training_file" "$ld_blocks_lasso" "$chromosome_col" "$pos_col" "$effect_allele" "$noneffect_allele" "$N_col" "$out_lasso" "$training_file_prefix" "$lambda_values" "$out_comparison" "$out_lassosum_on_set" "$out_SNPs_per_set"
+fi
+
+#Run PRS-CS on every gene set
+if [ "$gene_set" == "TRUE" ]; then
+    ./PRScs_on_set.bash "$PRScs_path" "$edited_GWAS_for_PRS_CS" "$test_file_rsID" "$test_file_prefix" "$training_file_rsID" "$training_file_prefix" "$out_PRScs_on_set" "$reference_files_PRScs" "$GWAS_size" "$out_comparison" "$out_SNPs_per_set"
+fi
+
+#Run LDPred-2 and lassosum2 on every gene set
+if [ "$gene_set" == "TRUE" ]; then
+    Rscript LDpred2_and_lasso2_on_set.r "$edited_GWAS_for_LDpred2" "$training_file_prefix" "$training_file_rsID" "$test_file_prefix" "$test_file_rsID" "$rsID_col" "$chromosome_col" "$pos_col" "$effect_allele" "$noneffect_allele" "$N_col" "$beta_se_col" "$allele_freq_col" "$out_LDpred2_on_set" "$out_lasso2_on_set" "$ldref_hm3_plus" "$cores" "$out_SNPs_per_set" "$out_comparison" "$lambda_min_ratio"   
+fi
+
+#Get best gene set
+if [ "$gene_set" == "TRUE" ]; then
+    if [ "$binary_trait" == "TRUE" ]; then
+        Rscript get_best_set.r "$out_PRSet" "$out_PRSice_on_Set" "$out_lassosum_on_set" "$out_PRScs_on_set" "$out_LDpred2_on_set" "$out_lasso2_on_set" "$out_comparison" "$test_file_prefix" "$test_file_rsID_prefix" "$training_file_prefix" "$training_file_rsID_prefix" "$PC_test" "$PC_training" "$pheno" "$covariates" "$cov_file" "$cross_validation"
+    elif [ "$binary_trait" == "FALSE" ]; then
+	    Rscript get_best_set_linear.r "$out_PRSet" "$out_PRSice_on_Set" "$out_lassosum_on_set" "$out_PRScs_on_set" "$out_LDpred2_on_set" "$out_lasso2_on_set" "$out_comparison" "$test_file_prefix" "$test_file_rsID_prefix" "$training_file_prefix" "$training_file_rsID_prefix" "$PC_test" "$PC_training" "$pheno" "$covariates" "$cov_file" "$cross_validation"
+    else 
+	    echo "Indicate if trait is binary (TRUE) or not (FALSE)"
+    fi  
+fi
